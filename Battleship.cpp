2d2e7e859529a8fc4bc5bb94e6ae7abd0b9f9890
Sushiii-1 <143401923+Sushiii-1@users.noreply.github.com>
2023-09-08 16:18:03 +0200

    Add files via upload
    an improved version of initial game. Added cpu turn so player stakes are high.
    made it so only between 1 and 5 is accepted for row and col  so turn is not missed.
    dunno how to improve cpu placement time...

--- Battleship.cpp
+++ Battleship.cpp
@@ -3,9 +3,27 @@
 
 using namespace std;
 
-int row, column, NumberTurns = 0, hits = 0, play;
+int row, column, NumberTurns = 0, userhits = 0,cpuhits = 0, play, shipload;
 
-bool ship[5][5]={
+void rowCHECK(){ //check if row is valid number
+
+    while(row < 1 || row > 5){
+        cout << "\nINVALID! PLEASE PICK BETWEEN 1 AND 5!\n";
+        cout << endl << "Row:";
+        cin >> row;
+    }
+}
+
+void columnCHECK(){ //check if column is valid number
+
+    while(column < 1 || column > 5){
+        cout << "\nINVALID! PLEASE PICK BETWEEN 1 AND 5!\n";
+        cout << endl << "Column:";
+        cin >> column;
+    }
+}
+
+bool ship[5][5]={ //computers grid
 {0,0,0,0,0}, //1
 {0,0,0,0,0}, //2
 {0,0,0,0,0}, //3
@@ -13,7 +31,15 @@ bool ship[5][5]={
 {0,0,0,0,0}, //5
 };
 
-char grid[5][5]={
+bool playerSHIPS[5][5]={ // players grid
+{0,0,0,0,0}, //1
+{0,0,0,0,0}, //2
+{0,0,0,0,0}, //3
+{0,0,0,0,0}, //4
+{0,0,0,0,0}, //5
+};
+
+char userGRID[5][5]={ //players grid
 {'O','O','O','O','O'}, 
 {'O','O','O','O','O'}, 
 {'O','O','O','O','O'}, 
@@ -21,21 +47,25 @@ char grid[5][5]={
 {'O','O','O','O','O'},     
 };
 
-void comSHIP(){
+void userSIDE(){ //print users grid
 
-    int shipC = 0;
-    while (shipC != 8){
-        srand(time(0));
-        int compr = 0 + rand() % 4;
-        int compc = 0 + rand() % 4;
-        if(ship[compr][compc]  == false){
-            ship[compr][compc] =  true;
-            shipC++;
+        for(int x = 0; x < 5;x++){
+        for(int y = 0; y < 5;y++){
+            cout << userGRID[x][y] << " ";
         }
+        cout << endl;
     }
 }
 
-void playerGRID(){
+char grid[5][5]={ // grid to show cpu's grid 
+{'O','O','O','O','O'}, 
+{'O','O','O','O','O'}, 
+{'O','O','O','O','O'}, 
+{'O','O','O','O','O'}, 
+{'O','O','O','O','O'},     
+};
+
+void cpuGRID(){ // print cpu's grid
 
     for(int x = 0; x < 5;x++){
         for(int y = 0; y < 5;y++){
@@ -45,7 +75,50 @@ void playerGRID(){
     }
 }
 
-int again(){
+void comSHIP(){ //computer placing ships
+
+    cout << "\nComputer is placing ships...\n";
+    int shipC = 0;
+    while (shipC != 8){
+        srand(time(0));
+        int compr = 0 + rand() % 4;
+        int compc = 0 + rand() % 4;
+        if(ship[compr][compc]  == false){
+            ship[compr][compc] =  true;
+            shipC++;
+            shipload++;
+            cout << "Placing ships..." << shipload << "/8" << endl;
+        }
+    }
+    cout << "\nComputer is done\nCommencing game...\n\n";
+}
+
+void playerSHIP(){ //player placing ships
+
+    int placedSHIPS = 8;
+    while (placedSHIPS != 0){
+        cout << "\nPlease place " << placedSHIPS << " ships anywhere on the grid,\n";
+        userSIDE();
+        cout << "Row:";
+        cin >> row;
+        rowCHECK();
+        row--;
+        cout << "Column:";
+        cin >> column;
+        columnCHECK();
+        column--;
+        if(playerSHIPS[row][column] == false){
+            playerSHIPS[row][column] = true;
+            userGRID[row][column] = '|';
+            placedSHIPS--;
+        }
+        else{
+            cout << "\nShip already present!\n";
+        }
+    }
+}
+
+int again(){ // play again
     cout << "Would you like to play again?\n";
     cout << "1.Yes\\2.No" << endl;
     cin >> play;
@@ -56,39 +129,71 @@ int again(){
     return play;
 }
 
-int main(){
+void cpuTURN(){ //cpu
 
-    cout << "-------------------" << endl;
-    cout << "Welcome to Battleship!" << endl;
+    cout << "\n---CPU's TURN---\n";
+    srand(time(NULL));
+    int compr = 0 + rand() % 4;
+    int compc = 0 + rand() % 4;
+    if(userGRID[compr][compc] == 'X'){
+        cpuTURN();  
+    }
+    if(playerSHIPS[compr][compc] ==  true){
+        cout << "DIRECT HIT BY CPU!\n";
+        playerSHIPS[compr][compc] = false;
+        userGRID[compr][compc] = 'X';
+        cpuhits++; 
+    }
+    else {
+        cout << "CPU MISSED!";
+        userGRID[compr][compc] = 'X'; 
+    }
+}
 
-    comSHIP();
+void game(){ // actual game
 
-    while(hits < 8){
-        cout << "Please choose where you would like to strike:" << endl;
-        playerGRID();
+    while(userhits != 8 || cpuhits != 8){
+        cout << endl << "Please choose where you would like to strike:" << endl;
+        cout << "-------------------CPU SIDE------------------" << endl;
+        cpuGRID();
+        cout << endl;
+        cout << "-----------------PLAYER SIDE------------------" << endl;
+        userSIDE();
+        cout << endl;
         cout << endl << "Row: ";
         cin >> row;
+        rowCHECK();
         row--;
         cout << "Column: ";
         cin >> column;
+        columnCHECK();
         column--;
         if(ship[row][column] == true){
             ship[row][column] = false;
             grid[row][column] = 'X';
             cout << endl << "Direct hit!" << endl;
-            hits++;
-            cout << (8-hits) << " ships are remaining!" << endl;
+            userhits++;
+            cout << (8-userhits) << " ships are remaining!" << endl;
             NumberTurns++;
         }
         else{
             cout << endl << "You missed!" << endl;
             grid[row][column] = '0';
-            cout << (8-hits) << " ships are remaining!" << endl;
+            cout << (8-userhits) << " ships are remaining!" << endl;
             NumberTurns++;
         }
+        cpuTURN();
+    }
 }
 
-    cout << "You won in " << NumberTurns << " Turns." << endl;
+int main(){
+
+    cout << "-------------------" << endl;
+    cout << "Welcome to Battleship!" << endl;
+
+    playerSHIP();
+    comSHIP();
+    game();
 
     again();
 
